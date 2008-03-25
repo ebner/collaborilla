80044ff77c588da112a36976d406302f2e5f85d8
Hannes Ebner <hannes.ebner@gmail.com>
2008-03-25 11:19:23 +0000

    added getters for description, purpose, context

--- src/se/kth/nada/kmr/collaborilla/ldap/SearchResultEntry.java
+++ src/se/kth/nada/kmr/collaborilla/ldap/SearchResultEntry.java
@@ -3,18 +3,30 @@ package se.kth.nada.kmr.collaborilla.ldap;
 import java.io.StringReader;
 import java.util.Date;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.NodeIterator;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.rdf.model.impl.PropertyImpl;
 import com.hp.hpl.jena.vocabulary.DC;
 
 public class SearchResultEntry implements Comparable<SearchResultEntry> {
+	
+	static Log log = LogFactory.getLog(SearchResultEntry.class);
+	
+	private static String ulm = "http://kmr.nada.kth.se/rdf/ulm#";
+	
+	private static String purpose = ulm + "purpose";
+	
+	private static String context = ulm + "context";
 
 	private String uri;
 	
@@ -31,9 +43,6 @@ public class SearchResultEntry implements Comparable<SearchResultEntry> {
 		this.metadata = metadata;
 		this.creationDate = creationDate;
 		this.modificationDate = modificationDate;
-		if (uri != null && metadata != null) {
-			model = readModel(metadata, uri);
-		}
 	}
 	
 	public int compareTo(SearchResultEntry sr) {
@@ -61,6 +70,7 @@ public class SearchResultEntry implements Comparable<SearchResultEntry> {
 	}
 	
 	public String getTitle() {
+		initModel();
 		if (model == null) {
 			return null;
 		}
@@ -68,6 +78,7 @@ public class SearchResultEntry implements Comparable<SearchResultEntry> {
 	}
 	
 	public String getAuthorName() {
+		initModel();
 		if (model == null) {
 			return null;
 		}
@@ -79,13 +90,41 @@ public class SearchResultEntry implements Comparable<SearchResultEntry> {
 	}
 	
 	public String getDescription() {
+		initModel();
 		if (model == null) {
 			return null;
 		}
-		return getDescription(model, uri, "en");
+		return getPropertyAlt(model, DC.description, uri, "en");
+	}
+	
+	public String getPurpose() {
+		initModel();
+		if (model == null) {
+			return null;
+		}
+		return getPropertyAlt(model, model.createProperty(purpose), uri, "en");
+	}
+	
+	public String getContext() {
+		initModel();
+		if (model == null) {
+			return null;
+		}
+		return getPropertyAlt(model, model.createProperty(context), uri, "en");
 	}
 	
 	/* RDF helpers */
+	
+	private void initModel() {
+		if ((uri != null) && (metadata != null) && (model == null)) {
+			model = readModel(metadata, uri);
+			if (model != null) {
+				log.debug("Model initialized");
+			} else {
+				log.error("Could not initialize model");
+			}
+		}
+	}
 	
 	private static Model readModel(String rdfString, String uri) {
 		Model m = ModelFactory.createDefaultModel();
@@ -95,11 +134,10 @@ public class SearchResultEntry implements Comparable<SearchResultEntry> {
 	}
 	
     private static String getTitle(Model model, String uri, String language) {
-    	String title = "http://purl.org/dc/elements/1.1/title";
     	String none = null;
         String fallback = null;
         String other = null;
-    	NodeIterator nodes = model.listObjectsOfProperty(model.createResource(uri), model.createProperty(title));
+    	NodeIterator nodes = model.listObjectsOfProperty(model.createResource(uri), DC.title);
     	while (nodes.hasNext()) {
     		RDFNode node = nodes.nextNode();
     		if (node instanceof Literal) {
@@ -128,39 +166,29 @@ public class SearchResultEntry implements Comparable<SearchResultEntry> {
     	return uri;
     }
     
-    private static String getDescription(Model model, String uri, String language) {
-    	String description = "http://purl.org/dc/elements/1.1/description";
-    	String none = null;
-        String fallback = null;
-        String other = null;
-    	NodeIterator nodes = model.listObjectsOfProperty(model.createResource(uri), model.createProperty(description));
-    	while (nodes.hasNext()) {
-    		RDFNode node = nodes.nextNode();
-    		if (node instanceof Literal) {
-    			String actualLanguage = ((Literal) node).getLanguage();
-    			String literal = ((Literal) node).getString();
-                boolean aLNull = actualLanguage == null || actualLanguage.length() == 0;
-                if (aLNull) {
-                	none = literal;
-                } else if (actualLanguage.equals(language)) {
-                    return literal;
-                } else if (actualLanguage.equals("en")) {
-                    fallback = literal;
-                } else {
-                	other = literal;
-                }
-    		}
-    	}
-    	if (fallback != null) {
-            return fallback;
-        } else if (none != null) {
-            return none;
-        }
-    	if (other != null) {
-    		return other;
-    	}
-    	return uri;
-    }    
+    private static String getPropertyAlt(Model model, Property res, String uri, String language) {
+		NodeIterator descIterator;
+		RDFNode descNode;
+		String result = null;
+		descIterator = model.listObjectsOfProperty(model.createResource(uri), res);
+
+		while (descIterator.hasNext()) {
+			descNode = descIterator.nextNode();
+			if (descNode instanceof Literal) {
+				Literal lit = (Literal) descNode;
+				if (lit.getLanguage() != null && lit.getLanguage().equals(language)) {
+					result = lit.getString();
+				}
+			} else if (descNode instanceof Resource) {
+				result = model.getAlt((Resource) descNode).getDefaultString();
+			}
+		}
+
+		if (result != null) {
+			return result;
+		}
+		return "No description available";
+    }
 	
     private static String getAuthorName(Model model) {
     	Property foafName = new PropertyImpl("http://xmlns.com/foaf/0.1/name");
